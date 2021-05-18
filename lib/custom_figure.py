7ac1dabba680858addc5f4ea0c261ae200f8539d
jdaguilar <jorgeaguilar2761@gmail.com>
2021-05-17 23:31:09 -0500

    change layout size

--- lib/custom_figure.py
+++ lib/custom_figure.py
@@ -4,10 +4,10 @@ import plotly.graph_objs as go
 def get_custom_indicator(value, delta=None):
 
     figure = go.Indicator(
-        mode = "number+delta",
+        mode="number+delta",
         value=value,
-        number={'valueformat': "000,000,000"},
-        delta = {
+        number={'valueformat': "000,000,000","font":{'size':40}},
+        delta={
             "reference": delta,
             "valueformat": "000,000,000",
         },
